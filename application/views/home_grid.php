d0a5d321725b48d95bf9dc4aa8239509b8745ff7
anonymouseyusuf <yusufprogramer1@gmail.com>
2019-04-09 12:23:43 +0700

    ganti bg gambar

--- application/views/home_grid.php
+++ application/views/home_grid.php
@@ -218,9 +218,9 @@
         					<aside class="wedget__categories sidebar--banner">
 								<img src="<?php echo base_url().'assets/home/images/others/banner_left.jpg'?>" alt="banner images">
 								<div class="text">
-									<h2>new products</h2>
+									<!-- <h2>new products</h2>
 									<h6>save up to <br> <strong>40%</strong>off</h6>
-								</div>
+ -->								</div>
         					</aside>
         				</div>
         			</div>
